03b4c8b73b9686be387a61f47314c229377f0e0b
Fabricio <fabricioliquesley15@gmail.com>
2024-02-23 22:20:26 -0300

    feat: create the global styles and add the theme of aplication

---
+++ src/styles/global.js
@@ -0,0 +1,97 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    :root {
+        --poppins: "Poppins", sans-serif;
+        --roboto: "Roboto", sans-serif;
+
+        font-size: 62.5%;
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.COLORS.DARK.DARK400};
+        font-size: 1.2rem;
+    }
+
+    body, input, button {
+        font-family: var(--poppins);
+        font-weight: 400;
+        outline: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.9);
+    }
+
+    .poppins-regular {
+        font-family: "Poppins", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .poppins-thin {
+        font-family: "Poppins", sans-serif;
+        font-weight: 100;
+        font-style: normal;
+    }
+
+    .poppins-extralight {
+        font-family: "Poppins", sans-serif;
+        font-weight: 200;
+        font-style: normal;
+    }
+
+    .poppins-light {
+        font-family: "Poppins", sans-serif;
+        font-weight: 300;
+        font-style: normal;
+    }
+
+    .poppins-medium {
+        font-family: "Poppins", sans-serif;
+        font-weight: 500;
+        font-style: normal;
+    }
+
+    .roboto-regular {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+    }
+
+    .roboto-bold {
+        font-family: "Roboto", sans-serif;
+        font-weight: 700;
+        font-style: normal;
+    }
+
+    .roboto-black {
+        font-family: "Roboto", sans-serif;
+        font-weight: 900;
+        font-style: normal;
+    }
+
+
+
+    /* @media (prefers-color-scheme: dark){
+        body {
+            background-color: #222222;
+            color: white;
+        }
+    } */
+`;
---
+++ src/styles/theme.js
@@ -0,0 +1,35 @@
+export default {
+    COLORS: {
+        LIGHT: {
+            LIGHT100: "#FFFFFF",
+            LIGHT200: "#FFFAF1",
+            LIGHT300: "#E1E1E6",
+            LIGHT400: "#C4C4CC",
+            LIGHT500: "#7C7C8A",
+            LIGHT600: "#76797B",
+            LIGHT700: "#4D585E",
+        },
+        DARK: {
+            DARK100: "#000405",
+            DARK200: "#00070A",
+            DARK300: "#000204",
+            DARK400: "#000A0F",
+            DARK500: "#000C12",
+            DARK600: "#00111A",
+            DARK700: "#001119",
+            DARK800: "#0D161B",
+            DARK900: "#0D1D25",
+            DARK1000: "#192227 ",
+        },
+        TINTS: {
+            TOMATO100: "#750310",
+            TOMATO200: "#92000E",
+            TOMATO300: "#AB222E",
+            TOMATO400: "#AB4D55",
+            CARROT100: "#FBA94C",
+            MINT100: "#04D361",
+            CAKE100: "#065E7C",
+            CAKE200: "#82F3FF"
+        }
+    }
+}
