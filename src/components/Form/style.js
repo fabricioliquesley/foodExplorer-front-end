8c6bde80b49d1b3c5e531751793915131c8107ad
Fabricio <fabricioliquesley15@gmail.com>
2024-02-24 23:11:54 -0300

    feat: update the form component to adapt to different screens

--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { DEVICE_BREAKPOINT } from "../../styles/deviceBreakPoint";
 
 export const Container = styled.form`
     display: flex;
@@ -9,6 +10,10 @@ export const Container = styled.form`
     padding: 0 3rem;
     /* margin-bottom: 10rem; */
 
+    > h2 {
+        display: none;
+    }
+
     fieldset {
         border: none;
 
@@ -16,7 +21,25 @@ export const Container = styled.form`
             display: block;
             color: ${({theme}) => theme.COLORS.LIGHT.LIGHT400};
             font-size: 1.1rem;
-            margin-bottom: 0.5rem;
+            margin-bottom: 0.8rem;
+        }
+    }
+
+    > a {
+        color: ${({theme}) => theme.COLORS.LIGHT.LIGHT100};
+        text-align: center;
+        font-size: 1.1rem;
+    }
+
+    @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+        background-color: ${({theme}) => theme.COLORS.DARK.DARK700};
+        padding: 6.4rem;
+        border-radius: 1.6rem;
+
+        > h2 {
+            display: block;
+            text-align: center;
+            color: ${({theme}) => theme.COLORS.LIGHT.LIGHT100};
         }
     }
 `;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SignUp } from "./pages/signUp"
 import GlobalStyle from "./styles/global";
 import { ThemeProvider } from 'styled-components';
+import { Routes } from "./routes/routes";
 
 import theme from './styles/theme';
 
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <SignUp />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
