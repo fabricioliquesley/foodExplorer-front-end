b7dcb4b2a194f09c155b299b17e8851be259ee8f
Fabricio <fabricioliquesley15@gmail.com>
2024-02-27 14:01:03 -0300

    feat: finish the slider component

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,25 +1,104 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { Container } from './style';
+import { Button } from "../Button";
+import { HiMiniMinus, HiMiniPlus } from "react-icons/hi2";
+import { CiHeart } from "react-icons/ci";
 
-// Import Swiper styles
-import 'swiper/css';
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+
+import { useState } from 'react';
 
 export function Slider({ title, data }) {
+    const [sliderRef, instanceRef] = useKeenSlider({
+        breakpoints: {
+            "(max-width: 320px)": {
+                slides: {
+                    perView: 1.3,
+                    spacing: 16
+                }
+            },
+            "(min-width: 321px)": {
+                slides: {
+                    perView: 1.4,
+                    spacing: 16
+                }
+            },
+            "(min-width: 420px)": {
+                slides: {
+                    perView: 1.6,
+                    spacing: 16
+                }
+            },
+            "((min-width: 426px) and (max-width: 600px))": {
+                slides: {
+                    perView: 2,
+                    spacing: 16
+                }
+            },
+            "((min-width: 600px) and (max-width: 768px))": {
+                slides: {
+                    perView: 2.5,
+                    spacing: 16
+                }
+            },
+            "(min-width: 769px)": {
+                slides: {
+                    perView: 4,
+                    spacing: 16
+                }
+            },
+        }
+    })
+
+    let [itemAmount, setItemAmount] = useState(0);
+
+    function decrementItemAmount() {
+        if (itemAmount > 0) {
+            setItemAmount(itemAmount -= 1);
+        }
+    }
+
+    function incrementItemAmount() {
+        setItemAmount(itemAmount += 1);
+    }
+
     return (
         <Container>
-            <h3>{title}</h3>
-            <Swiper
-                className='slider'
-                spaceBetween={20}
-                slidesPerView={2}
-            >
+            <h3 className='poppins-medium'>{title}</h3>
+            <div ref={sliderRef} className="keen-slider slider">
                 {
                     data.map(meal => (
-                        <SwiperSlide className='slide'>{meal.name}</SwiperSlide>
+                        <div className="keen-slider__slide slide">
+                            <button className='favoriteBtn'>
+                                <CiHeart />
+                            </button>
+                            <img src={meal.image_path} alt="Imagem ilustrativa do prato" />
+                            <p className='title poppins-medium'>
+                                {meal.name}
+                            </p>
+                            <span className='preco roboto-regular'>
+                                R$ {String(meal.preco.toFixed(2)).replace(".", ",")}
+                            </span>
+                            <div>
+                                <button
+                                    onClick={() => decrementItemAmount()}
+                                >
+                                    <HiMiniMinus />
+                                </button>
+                                <span className='roboto-regular'>
+                                    {String(itemAmount).padStart(2, "0")}
+                                </span>
+                                <button
+                                    onClick={() => incrementItemAmount()}
+                                >
+                                    <HiMiniPlus />
+                                </button>
+                            </div>
+                            <Button title={"incluir"} />
+                        </div>
                     ))
                 }
-            </Swiper>
+            </div>
         </Container>
-    );
-};
+    )
+}
--- src/components/Slider/style.js
+++ src/components/Slider/style.js
@@ -9,17 +9,78 @@ export const Container = styled.div`
     padding-left: 2rem;
     margin-top: 3.2rem;
 
+    > h3, .title, button, span {
+        color: ${({ theme }) => theme.COLORS.LIGHT.LIGHT300};
+    }
+
     > .slider {
         width: 100%;
 
         .slide {
-            max-width: 20rem;
-            height: 20rem;
-            background: ${({theme}) => theme.COLORS.DARK.DARK200};
-            border: 1px solid ${({theme}) => theme.COLORS.DARK.DARK300};
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1.2rem;
+            width: 20rem;
+            height: fit-content;
+            background: ${({ theme }) => theme.COLORS.DARK.DARK200};
+            border: 1px solid ${({ theme }) => theme.COLORS.DARK.DARK300};
             border-radius: 0.8rem;
+            padding: 2.4rem;
             cursor: pointer;
+
+            .favoriteBtn {
+                position: absolute;
+                top: 1.6rem;
+                right: 1.6rem;
+                width: fit-content;
+                display: grid;
+                place-content: center;
+                background: transparent;
+                border: none;
+                font-size: 2.5rem;
+            }
+
+            img {
+                width: 8.8rem;
+            }
+
+            .title {
+                font-size: 1.4rem;
+                text-align: center;
+                line-height: 2.4rem;
+
+                &::after {
+                    content: " >";
+                }
+            }
+
+            .preco {
+                color: ${({ theme }) => theme.COLORS.TINTS.CAKE200};
+                font-size: 1.6rem;
+            }
+
+            div {
+                display: flex;
+                align-items: center;
+                gap: 1.4rem;
+
+                button {
+                    display: grid;
+                    place-content: center;
+                    background: transparent;
+                    border: none;
+                    font-size: 2.5rem;
+                }
+
+                span {
+                    font-size: 1.6rem;
+                }
+            }
+
+            button {
+                width: 100%;
+            }
         }
     }
-
 `;
