07017520ed724f5fc3804620e7aa302997af80ed
Fabricio <fabricioliquesley15@gmail.com>
2024-02-28 23:11:51 -0300

    feat: adapting the banner component for different screen sizes

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,13 @@
 import { Container } from "./style";
-import bannerImg from "../../assets/banner_image_sm.png";
+import bannerImg from "../../assets/banner_image.png";
 
-export function Banner(){
+export function Banner() {
     return (
         <Container>
-            <img src={bannerImg} alt="Imagem ilustrativa de frutas e biscoitos" />
-            <div>
+            <div className="imgContainer">
+                <img src={bannerImg} alt="Imagem ilustrativa de frutas e biscoitos" />
+            </div>
+            <div className="text">
                 <h2>Sabores inigualáveis</h2>
                 <p>Sinta o cuidado do preparo com ingredientes selecionados.</p>
             </div>
--- src/components/Banner/style.js
+++ src/components/Banner/style.js
@@ -1,27 +1,39 @@
 import { styled } from "styled-components";
+import { DEVICE_BREAKPOINT } from "../../styles/deviceBreakPoint";
 
 export const Container = styled.div`
     position: relative;
     display: flex;
-    background: ${({theme}) => theme.COLORS.GRADIENTS.GRADIENTS200};
+    align-items: center;
+    justify-content: space-between;
+    background: ${({ theme }) => theme.COLORS.GRADIENTS.GRADIENTS200};
     border-radius: 0.2917rem;
-    margin: 2rem;
+    margin: 4.4rem 2rem;
     padding: 1rem;
-    
-    > img {
+
+    > .imgContainer {
         position: absolute;
-        opacity: 0.8;
         bottom: 0;
-        left: -15px;
-        width: 10rem;
+        left: -3rem;
+        width: 15rem;
+        height: 15rem;
+        overflow: hidden;
+
+        > img {
+            position: absolute;
+            width: 20rem;
+            bottom: -15rem;
+            opacity: 0.8;
+            transform: scaleX(-1);
+        }
     }
     
-    > div {
-        margin-left: 6rem;
-        width: 100%;
-        max-width: 20rem;
-        color: ${({theme}) => theme.COLORS.LIGHT.LIGHT300};
+    > .text {
+        width: 20rem;
+        color: ${({ theme }) => theme.COLORS.LIGHT.LIGHT300};
         z-index: 1;
+        margin: 0 1rem 0 auto;
+        /* background: red; */
 
         h2 {
             font-size: 1.8rem;
@@ -29,6 +41,43 @@ export const Container = styled.div`
 
         p {
             font-size: 1.2rem;
+        }
+    }
+
+    @media (min-width: ${DEVICE_BREAKPOINT.SM}) {
+        > .text {
+            margin: 0 auto;
+        }
+    }
+
+    @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+        height: 26rem;
+        margin: 9rem 0 0;
+        padding: 0;
+
+        > .imgContainer {
+            left: -4rem;
+            width: 49rem;
+            height: 34.3rem;
+
+            > img {
+                bottom: -30rem;
+                width: 50rem;
+            }
+        }
+
+        > .text {
+            margin-left: 25rem;
+            width: max-content;
+
+            h2 {
+                font-size: 4rem;
+                line-height: 140%;
+            }
+
+            p {
+                font-size: 1.6rem;
+            }
         }
     }
 `;
