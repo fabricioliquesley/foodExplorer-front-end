cfa98564188160094960d211dbbab72be9a289b4
Fabricio <fabricioliquesley15@gmail.com>
2024-02-28 23:15:40 -0300

    feat: adapting the homepage layout for different screens

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,6 +10,12 @@ import { useState } from "react";
 export function Home() {
     const [statusMenu, setStatusMenu] = useState("close");
 
+    const [width, setWidth] = useState(window.document.defaultView.innerWidth);
+
+    window.addEventListener("resize", () => {
+        setWidth(window.document.defaultView.innerWidth);
+    })
+
     function toggleMenu() {
         if (statusMenu == "open") {
             return setStatusMenu("close");
@@ -71,6 +77,19 @@ export function Home() {
             preco: 19.95,
             description: "Um delicioso prato feito com ingredientes de extrema qualidade, serve 1 pessoa. Acompanha arroz e batata palha."
         },
+        {
+            image_path: "/src/assets/mail_example.png",
+            name: "Strogonoff de Camarão",
+            category: "Prato Principal",
+            ingredients: [
+                "carne",
+                "creme de leite",
+                "molho de tomate",
+                "cogumelos champignon"
+            ],
+            preco: 19.95,
+            description: "Um delicioso prato feito com ingredientes de extrema qualidade, serve 1 pessoa. Acompanha arroz e batata palha."
+        },
     ]
 
     return (
@@ -81,11 +100,14 @@ export function Home() {
                 onClick={() => toggleMenu()}
             />
             <main>
-                <Menu status={statusMenu} />
+                {
+                    width < 1024 &&
+                    <Menu status={statusMenu} />
+                }
                 <Banner />
-                <Slider title={"Refeições"} data={data}/>
-                <Slider title={"Pratos principais"} data={data}/>
-                <Slider title={"Bebidas"} data={data}/>
+                <Slider title={"Refeições"} data={data} />
+                <Slider title={"Pratos principais"} data={data} />
+                <Slider title={"Bebidas"} data={data} />
             </main>
             <Footer />
         </Container>
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,14 +1,22 @@
 import { styled } from "styled-components";
+import { DEVICE_BREAKPOINT } from "../../styles/deviceBreakPoint";
 
 export const Container = styled.div`
     display: grid;
-    grid-template-rows: 11.4rem 1fr max-content;
+    grid-template-rows: max-content 1fr max-content;
     width: 100%;
     height: 100vh;
+    position: relative;
+    overflow: hidden;
     
     > main {
-        overflow: ${({$statusMenu}) => $statusMenu == "open" ? "hidden" : "auto"};
-        position: relative;
+        overflow: ${({ $statusMenu }) => $statusMenu == "open" ? "hidden" : "auto"};
         padding-bottom: 6.2rem;
+    }
+
+    @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+        > main {
+            padding: 0 12.3rem 6.2rem;
+        }
     }
 `;
