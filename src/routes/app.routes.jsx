e967b932e640ac048b4f762aebcc2c3cc56a52ec
Fabricio <fabricioliquesley15@gmail.com>
2024-02-29 22:32:19 -0300

    feat: create meal details route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
+import { MealDetails } from "../pages/MealDetails";
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/meal/:id" element={<MealDetails />} />
         </Routes>
     )
 }
