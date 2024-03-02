3f1667dc02015bc03ecfa986ce91c20582e83b86
Fabricio <fabricioliquesley15@gmail.com>
2024-03-02 11:30:16 -0300

    feat: create a desktop layout for the meal creation page and a function to add a tag

--- src/pages/CreateMeal/index.jsx
+++ src/pages/CreateMeal/index.jsx
@@ -7,14 +7,18 @@ import { FormInput } from "../../components/FormInput";
 import { TextArea } from "../../components/TextArea";
 import { Select } from "../../components/Select";
 import { Tags } from "../../components/Tags";
+import { Tag } from "../../components/Tag";
 
 import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
 
 import { useState } from "react";
 
 export function CreateMeal() {
     const [statusMenu, setStatusMenu] = useState("close");
+    const [ingredients, setIngredients] = useState([]);
+    const [inputValue, setInputValue] = useState("");
 
     function toggleMenu() {
         if (statusMenu == "open") {
@@ -24,10 +28,22 @@ export function CreateMeal() {
         return setStatusMenu("open");
     }
 
-    const ingredients = [
-        "frango",
-        "milho"
-    ]
+    function addIngredientTag() {
+        if (!inputValue) {
+            return
+        }
+
+        setIngredients(prevState => [...prevState, inputValue]);
+        setInputValue("");
+    }
+
+    function removeIngredientTag(tagToDelete) {
+        setIngredients(prevState => prevState.filter(ingredient => ingredient !== tagToDelete));
+    }
+
+    function saveMeal() {
+        alert("Save")
+    }
 
     return (
         <Container $statusMenu={statusMenu}>
@@ -39,39 +55,63 @@ export function CreateMeal() {
             <main>
                 <Menu status={statusMenu} />
                 <Content>
-                    <Link to={-1}>
+                    <Link to={-1} className="link">
                         <IoIosArrowBack />
                         voltar
                     </Link>
-                    <h2>Novo prato</h2>
+                    <h2 className="title">Novo prato</h2>
                     <FormInput
                         label={"Imagem do prato"}
                         variant={"file"}
                         type={"file"}
                         placeholder={"Ex.: Salada Ceasar"}
+                        className={"imageInput"}
                     />
                     <FormInput
                         label={"Nome"}
                         type={"text"}
                         placeholder={"Ex.: Salada Ceasar"}
+                        className={"inputName"}
                     />
                     <Select
                         label={"Categoria"}
+                        className={"inputCategory"}
                     />
                     <Tags
                         label={"Ingredientes"}
-                        data={ingredients}
-                    />
+                        addClick={addIngredientTag}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        className={"ingredientsTag"}
+                    >
+                        {
+                            ingredients.map((tag, index) => (
+                                <Tag key={index}>
+                                    <p>{tag}</p>
+                                    <button onClick={() => removeIngredientTag(tag)}>
+                                        <IoCloseOutline size={20} />
+                                    </button>
+                                </Tag>
+                            ))
+                        }
+                    </Tags>
                     <FormInput
                         label={"Preço"}
                         type={"number"}
                         placeholder={"R$ 00,00"}
+                        className={"inputPreco"}
                     />
                     <TextArea
                         label={"Descrição"}
                         placeholder={"Fale brevemente sobre o prato, seus ingredientes e composição"}
+                        className={"textArea"}
                     />
-                    <Button title={"Salvar alterações"} />
+                    <Button
+                        title={"Salvar alterações"}
+                        variant
+                        onClick={saveMeal}
+                        className={"saveButton"}
+                    />
                 </Content>
             </main>
             <Footer />
