2c54da03322fc1966e8def7f5bea66ca54f069a2
Fabricio <fabricioliquesley15@gmail.com>
2024-03-01 21:53:48 -0300

    feat: create the route to create a new meal

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../pages/Home";
 import { MealDetails } from "../pages/MealDetails";
+import { CreateMeal } from "../pages/CreateMeal";
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/meal/:id" element={<MealDetails />} />
+            <Route path="/create" element={<CreateMeal />} />
         </Routes>
     )
 }
