d14d352ef53b34d6cbb1ef483c14b61ec6345ace
Fabricio <fabricioliquesley15@gmail.com>
2024-03-06 16:44:01 -0300

    feat: create route to complete an order

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -4,6 +4,7 @@ import { Home } from "../pages/Home";
 import { MealDetails } from "../pages/MealDetails";
 import { CreateMeal } from "../pages/CreateMeal";
 import { EditMeal } from "../pages/EditMeal";
+import { Order } from "../pages/Order";
 
 export function AppRoutes() {
     return (
@@ -12,6 +13,7 @@ export function AppRoutes() {
             <Route path="/meal/:id" element={<MealDetails />} />
             <Route path="/create" element={<CreateMeal />} />
             <Route path="/edit/:id" element={<EditMeal />} />
+            <Route path="/order" element={<Order />} />
         </Routes>
     )
 }
