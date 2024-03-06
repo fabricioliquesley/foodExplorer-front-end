301ecb6a2ca5b974872aee96625ea68d04230799
Fabricio <fabricioliquesley15@gmail.com>
2024-03-05 22:06:48 -0300

    feat: create a logo variant for when the user is an administrator

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,13 +1,15 @@
 import { Container } from "./style";
 import explorerLogo from "/explorer.svg"
 
-export function Logo({variant = false}){
+export function Logo({ variant = false }) {
     return (
         <Container $variant={variant}>
-            <img src={explorerLogo} alt="Logo do food explorer" />
-            <h1 className="roboto-bold">food explorer</h1>
+            <div>
+                <img src={explorerLogo} alt="Logo do food explorer" />
+                <h1 className="roboto-bold">food explorer</h1>
+            </div>
             {
-                variant=="admin" &&
+                variant == "admin" &&
                 <span className="roboto-regular">admin</span>
             }
         </Container>
--- src/components/Logo/style.js
+++ src/components/Logo/style.js
@@ -1,22 +1,44 @@
 import { styled } from "styled-components";
+import { DEVICE_BREAKPOINT } from "../../styles/deviceBreakPoint";
 
 export const Container = styled.div`
     display: flex;
     align-items: center;
-    gap: ${({$variant}) => $variant == "footer" ? "0.6rem" : "1rem"};
+    gap: ${({ $variant }) => $variant == "footer" ? "0.6rem" : "1rem"};
     width: fit-content;
+    
+    > div {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
 
-    > img {
-        filter: ${({$variant}) => $variant == "footer" ? "grayscale(1)" : "grayscale(0)"};
-        width: ${({$variant}) => $variant == "footer" ? "1.2rem" : "2.6rem"};
+        > img {
+            filter: ${({ $variant }) => $variant == "footer" ? "grayscale(1)" : "grayscale(0)"};
+            width: ${({ $variant }) => $variant == "footer" ? "1.2rem" : "2.6rem"};
+        }
+
+        h1 {
+            color: ${({ theme, $variant }) => $variant == "footer" ? theme.COLORS.LIGHT.LIGHT700 : theme.COLORS.LIGHT.LIGHT100};
+            font-size: ${({ $variant }) => $variant == "footer" ? "1.1rem" : "2.4rem"};
+        }
+    
+        @media (max-width: ${DEVICE_BREAKPOINT.XS}) {
+            h1 {
+                font-size: ${({ $variant }) => $variant == "footer" ? "1.1rem" : "2rem"};
+            }
+        }
     }
 
-    > h1 {
-        color: ${({theme, $variant}) => $variant == "footer" ? theme.COLORS.LIGHT.LIGHT700 : theme.COLORS.LIGHT.LIGHT100};
-        font-size: ${({$variant}) => $variant == "footer" ? "1.1rem" : "2.4rem"};
+    span {
+        color: ${({ theme }) => theme.COLORS.TINTS.CAKE200};
+        font-size: 1.2rem;
     }
 
-    > span {
-        color: ${({theme}) => theme.COLORS.TINTS.CAKE200};
+    @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+        flex-direction: column;
+        gap: 0;
+        align-items: flex-end;
     }
+
+
 `;
