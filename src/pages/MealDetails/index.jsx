ab58d1ef7937ce3673dc7eb396313505f70848c4
Fabricio <fabricioliquesley15@gmail.com>
2024-03-15 22:24:14 -0300

    feat: create the logic to show the details of a meal and create the function to add the quantity of the meal to the order

--- src/pages/MealDetails/index.jsx
+++ src/pages/MealDetails/index.jsx
@@ -2,21 +2,22 @@ import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Menu } from "../../components/Menu";
 import { Container, Content, Tags, Left, Right } from "./style";
-import { Link } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
 import { HiMiniMinus, HiMiniPlus } from "react-icons/hi2";
 import { PiReceipt } from "react-icons/pi";
 
 import { ACCOUNT_TYPE } from "../../utils/accountType";
 
-import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import { api } from "../../services/api";
 
 export function MealDetails() {
     const [statusMenu, setStatusMenu] = useState("close");
-
     const [width, setWidth] = useState(window.document.defaultView.innerWidth);
-
-    let [itemAmount, setItemAmount] = useState(0);
+    const [meal, setMeal] = useState(null);
+    const [mealQuantity, setMealQuantity] = useState("0");
 
     window.addEventListener("resize", () => {
         setWidth(window.document.defaultView.innerWidth);
@@ -30,39 +31,107 @@ export function MealDetails() {
         return setStatusMenu("open");
     }
 
-    function decrementItemAmount() {
-        if (itemAmount > 0) {
-            setItemAmount(itemAmount -= 1);
-        }
+    const [orderItems, setOrderItems] = useState(JSON.parse(localStorage.getItem("@foodExplorer:orderItems")) ?? []);
+
+    function fetchForOrderItem(name) {
+        if (!orderItems) return [false, null];
+
+        let result = false;
+        let index;
+
+        orderItems.map((orderItem, i) => {
+            if (orderItem.name == name) {
+                result = true;
+                index = i;
+            }
+        })
+
+        if (result) return [true, index];
+
+        return [false, null];
     }
 
     function incrementItemAmount() {
-        setItemAmount(itemAmount += 1);
+        const mealImgPath = document.querySelector(".mealImg").src;
+        const mealAmount = document.querySelector(".amount");
+
+        const [thereIsThisItemInTheOrder, index] = fetchForOrderItem(meal.name);
+
+        if (!thereIsThisItemInTheOrder) {
+            mealAmount.textContent++;
+
+            setOrderItems(prev => [...prev, {
+                name: meal.name,
+                img: mealImgPath,
+                price: meal.price,
+                amount: Number(mealAmount.textContent),
+            }])
+
+            return;
+        }
+
+        mealAmount.textContent++;
+        orderItems[index].amount = Number(mealAmount.textContent);
+
+        return;
     }
 
-    const meal = {
-        image_path: "/src/assets/mail_example.png",
-        name: "Strogonoff de Frango",
-        category: "Prato Principal",
-        ingredients: [
-            "frango",
-            "creme de leite",
-            "molho de tomate",
-            "cogumelos champignon"
-        ],
-        preco: 19.95,
-        description: "Um delicioso prato feito com ingredientes de extrema qualidade, serve 1 pessoa. Acompanha arroz e batata palha."
+    function decreaseItemQuantity() {
+        const mealAmount = document.querySelector(".amount");
+
+        const [thereIsThisItemInTheOrder, index] = fetchForOrderItem(meal.name);
+
+        if (!thereIsThisItemInTheOrder) {
+            return
+        }
+
+        if (orderItems[index].amount > 0) {
+            mealAmount.textContent--;
+            orderItems[index].amount = Number(mealAmount.textContent);
+
+            if (orderItems[index].amount == 0) {
+                setOrderItems(orderItems.filter(orderItem => orderItem.name != meal.name));
+            }
+        }
+    }
+
+    function addItemToOrder() {
+        return localStorage.setItem("@foodExplorer:orderItems", JSON.stringify(orderItems));
     }
 
     const user = {
         accountType: "adminc"
     }
 
+    const params = useParams();
+
+    async function fetchMeal() {
+        const { data: meal } = await api.get(`/meals/${params.id}`);
+
+        meal.image_path = `${api.defaults.baseURL}/files/${meal.image_path}`;
+
+        setMeal(meal);
+    }
+
+    useEffect(() => {
+        fetchMeal();
+    }, [])
+
+    useEffect(() => {
+        if (meal == null) return;
+
+        const [current] = orderItems.filter((p) => p.name == meal.name);
+
+        if (!current) return;
+
+        setMealQuantity(current.amount)
+    }, [meal])
+
     return (
         <Container $statusMenu={statusMenu}>
             <Header
                 menuStatus={statusMenu}
-                orderAmount={5}
+                orderAmount={orderItems.length}
                 onClick={() => toggleMenu()}
                 variant={user.accountType}
             />
@@ -80,55 +149,65 @@ export function MealDetails() {
                             <IoIosArrowBack />
                             voltar
                         </Link>
-                        <img src={meal.image_path} alt="Imagem ilustrativa do prato" />
-                    </Left>
-                    <Right>
-                        <div className="mealDetails">
-                            <h3 className="poppins-medium">{meal.name}</h3>
-                            <p className="poppins-light">
-                                {meal.description}
-                            </p>
-                            <div className="ingredientsContainer">
-                                {
-                                    meal.ingredients.map((ingredient, index) => (
-                                        <Tags key={index}>{ingredient}</Tags>
-                                    ))
-                                }
-                            </div>
-                        </div>
                         {
-                            [ACCOUNT_TYPE.ADMIN].includes(user.accountType) ?
-                                <Link 
-                                    to={"/edit/4f474d2f-eb71-4ac9-88c7-3a0aabb8e261"}
-                                    className="editBtn"
-                                >
-                                    Editar prato
-                                </Link>
-                                :
-                                <div className="amountControls">
-                                    <div>
-                                        <button
-                                            onClick={() => decrementItemAmount()}
-                                        >
-                                            <HiMiniMinus size={20} />
-                                        </button>
-                                        <span className='roboto-regular'>
-                                            {String(itemAmount).padStart(2, "0")}
-                                        </span>
-                                        <button
-                                            onClick={() => incrementItemAmount()}
-                                        >
-                                            <HiMiniPlus size={20} />
+                            meal &&
+                            <img
+                                className="mealImg"
+                                src={meal.image_path}
+                                alt="Imagem ilustrativa do prato"
+                            />
+                        }
+                    </Left>
+                    {
+                        meal &&
+                        <Right>
+                            <div className="mealDetails">
+                                <h3 className="poppins-medium">{meal.name}</h3>
+                                <p className="poppins-light">
+                                    {meal.description}
+                                </p>
+                                <div className="ingredientsContainer">
+                                    {
+                                        meal.ingredients.map((ingredient, index) => (
+                                            <Tags key={index}>{ingredient.name}</Tags>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                            {
+                                [ACCOUNT_TYPE.ADMIN].includes(user.accountType) ?
+                                    <Link
+                                        to={"/edit/4f474d2f-eb71-4ac9-88c7-3a0aabb8e261"}
+                                        className="editBtn"
+                                    >
+                                        Editar prato
+                                    </Link>
+                                    :
+                                    <div className="amountControls">
+                                        <div>
+                                            <button
+                                                onClick={() => decreaseItemQuantity()}
+                                            >
+                                                <HiMiniMinus size={20} />
+                                            </button>
+                                            <span className='roboto-regular amount'>
+                                                {mealQuantity}
+                                            </span>
+                                            <button
+                                                onClick={() => incrementItemAmount()}
+                                            >
+                                                <HiMiniPlus size={20} />
+                                            </button>
+                                        </div>
+                                        <button onClick={addItemToOrder}>
+                                            <PiReceipt size={20} />
+                                            pedir ∙
+                                            R$ {String(meal.price).replace(".", ",")}
                                         </button>
                                     </div>
-                                    <button>
-                                        <PiReceipt size={20} />
-                                        pedir ∙
-                                        R$ {String(meal.preco).replace(".", ",")}
-                                    </button>
-                                </div>
-                        }
-                    </Right>
+                            }
+                        </Right>
+                    }
                 </Content>
             </main>
             <Footer />
