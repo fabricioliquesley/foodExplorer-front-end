01f3967eb91177a0f674a13520bcf70069fa5b4b
Fabricio <fabricioliquesley15@gmail.com>
2024-03-18 22:58:28 -0300

    Fix: change the form input component for when a file is selected

--- src/components/FormInput/index.jsx
+++ src/components/FormInput/index.jsx
@@ -1,7 +1,7 @@
 import { Container } from "./style";
 import { FiUpload } from "react-icons/fi";
 
-export function FormInput({ label, variant, className, ...res }) {
+export function FormInput({ label, variant, text, className, ...res }) {
     return (
         <Container $variant={variant} className={className}>
             <label htmlFor="inputLabel">{label}</label>
@@ -10,7 +10,9 @@ export function FormInput({ label, variant, className, ...res }) {
                     variant == "file" &&
                     <>
                         <FiUpload size={24} />
-                        <p>Selecione imagem</p>
+                        {
+                            text ? <p>{text}</p> : <p>Selecione imagem</p>
+                        }
                     </>
                 }
                 <input type="text" id="inputLabel" {...res} />
